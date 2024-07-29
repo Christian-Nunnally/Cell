cd2e3220348df977c7cb57667afda45414a54e95
Christian Nunnally <christian.nunnally@ni.com>
2024-07-29 18:25:24 -0500

    .

---
+++ Cell/View/ToolWindow/CommandViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Input;
+
+namespace Cell.View.ToolWindow
+{
+    public class CommandViewModel(string name, ICommand command)
+    {
+        public string Name { get; set; } = name;
+
+        public ICommand Command { get; set; } = command;
+    }
+}
